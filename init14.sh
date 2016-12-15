c5c047a47b3ef5aed08142331a896051755dd7e3
Arnaud Garin <arnaud.garin@epfl.ch>
2016-12-15 17:53:08 +0100

    add init script

---
+++ init14.sh
@@ -0,0 +1,7 @@
+export QUARTUS_ROOTDIR=/home/amgarin/AOCL/altera/14.0/quartus
+export ALTERAOCLSDKROOT=/home/amgarin/AOCL/altera/14.0/hld
+export PATH="$QUARTUS_ROOTDIR"/bin:"$ALTERAOCLSDKROOT"/linux64/bin:"$ALTERAOCLSDKROOT"/bin:/home/amgarin/AOCL/altera/14.0/embedded/ds-5/bin:$PATH
+export LD_LIBRARY_PATH="$ALTERAOCLSDKROOT"/linux64/lib:"$ALTERAOCLSDKROOT"/host/linux64/lib:"$ALTERAOCLSDKROOT"/linux64_13.1/lib
+export AOCL_BOARD_PACKAGE_ROOT="$ALTERAOCLSDKROOT"/board/de1soc
+export QUARTUS_64BIT=1
+export LM_LICENSE_FILE=1719@lamipc54.epfl.ch
